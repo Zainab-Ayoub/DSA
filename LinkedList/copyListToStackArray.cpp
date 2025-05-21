76499e359e11e35ee5483a53a0e77fe345fa7542
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-05-21 13:20:34 +0500

    copy list to stack array

---
+++ LinkedList/copyListToStackArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* next;
+	Node(int val): data(val), next(NULL){};
+};
+
+void copyToStack(Node* head){
+	int stack[5], i=0;
+	while(head){
+		stack[i] = head->data;
+		cout<<stack[i]<<" ";
+		i++;                                                                                                    
+		head = head->next;
+	}
+}
+
+int main(){
+	Node* head = new Node(1);
+	head->next = new Node(2);
+	head->next->next = new Node(3);
+	head->next->next->next = new Node(4);
+	head->next->next->next->next = new Node(5);
+	cout<<"Values in Stack: ";
+	copyToStack(head);
+}
