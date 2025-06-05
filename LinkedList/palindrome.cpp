be3cec94f2873f151a3ef8c8659277f586f15781
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-06-06 00:25:10 +0500

    palindrome sequence

--- LinkedList/palindrome.cpp
+++ LinkedList/palindrome.cpp
@@ -5,66 +5,70 @@ struct Node{
     int data;
     Node* prev;
     Node* next;
-
     Node(int val): data(val), prev(nullptr), next(nullptr) {}
 };
 
 class DoublyLinkedList{
-    private:
-        Node* head;
-        Node* tail;
-    
-    public:
-        DoublyLinkedList(): head(nullptr), tail(nullptr) {}
+private:
+    Node* head;
+    Node* tail;
 
-        void insertAtBeginning(int val){
-            Node* newNode = new Node(val);
-            if(!head){
-                head = tail = newNode;
-            } else{
-                newNode->next = head;
-                head->prev = newNode;
-                head = newNode;
+public:
+    DoublyLinkedList(): head(nullptr), tail(nullptr) {}
+
+    void insertAtEnd(int val){
+        Node* newNode = new Node(val);
+        if(!tail){
+            head = tail = newNode;
+        } else{
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    bool isPalindrome(){
+        Node* left = head;
+        Node* right = tail;
+
+        while(left != nullptr && right != nullptr && left != right && left->prev != right){
+            if(left->data != right->data){
+                return false;
             }
+            left = left->next;
+            right = right->prev;
         }
+        return true;
+    }
 
-        void insertAtEnd(int val){
-            Node* newNode = new Node(val);
-            if(!tail){
-                head = tail = newNode;
-            } else{
-                tail->next = newNode;
-                newNode->prev = tail;
-                tail = newNode;
-            }
-        } 
-
-        void printForward(){
-            Node* temp = head;
-            while(temp){
-                cout<<temp->data<<" ";
-                temp = temp->next;
-            } cout<<endl; 
+    void printList(){
+        Node* temp = head;
+        while(temp){
+            cout<<temp->data<<" ";
+            temp=temp->next;
         }
-
-        void printBackward(){
-            Node* temp = tail;
-            while(temp){
-                cout<<temp->data<<" ";
-                temp = temp->prev;
-            } cout<<endl; 
-        }
+        cout<<endl;
+    }
 };
 
 int main(){
     DoublyLinkedList list;
+    int n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
 
-    list.insertAtBeginning(5);
-    list.insertAtEnd(10);
-    list.insertAtEnd(15);
-    list.insertAtEnd(20);
+    cout<<"Enter elements:\n";
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        list.insertAtEnd(x);
+    }
 
+    cout<<"List: ";
+    list.printList();
 
-
-
+    if(list.isPalindrome())
+        cout<<"The list is a palindrome.\n";
+    else
+        cout<<"The list is NOT a palindrome.\n";
 }
