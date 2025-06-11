0a7120661c26c41977b32ed7944bb4ee9b3e17d5
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-06-11 09:47:16 +0500

    commit

--- LinkedList/circularList.cpp
+++ LinkedList/circularList.cpp
@@ -1,31 +1,78 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+class Node {
+public:
     int data;
     Node* next;
+
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class CircularLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    CircularLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void createFromUserInput() {
+        int n, value;
+        cout << "Enter number of nodes: ";
+        cin >> n;
+
+        for (int i = 0; i < n; ++i) {
+            cout << "Enter value for node " << i + 1 << ": ";
+            cin >> value;
+            insertAtEnd(value);
+        }
+    }
+
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+            tail->next = head;  // Circular link
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+            tail->next = head;  // Maintain circular link
+        }
+    }
+
+    void display() {
+        if (!head) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        Node* temp = head;
+        cout << "Circular Linked List: ";
+        do {
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << endl;
+    }
+
+    ~CircularLinkedList() {
+        if (!head) return;
+
+        Node* temp = head->next;
+        while (temp != head) {
+            Node* nextNode = temp->next;
+            delete temp;
+            temp = nextNode;
+        }
+        delete head;
+    }
 };
 
 int main() {
-    Node* head = new Node{1, nullptr};
-    Node* second = new Node{2, nullptr};
-    Node* third = new Node{3, nullptr};
-
-    head->next = second;
-    second->next = third;
-    third->next = head; 
-
-    Node* temp = head;
-    cout << "Circular Linked List: ";
-    do {
-        cout << temp->data << " ";
-        temp = temp->next;
-    } while (temp != head);
-    cout << endl;
-
-    delete head;
-    delete second;
-    delete third;
+    CircularLinkedList cll;
+    cll.createFromUserInput();
+    cll.display();
 
     return 0;
 }
