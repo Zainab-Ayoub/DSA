a1c5b616f33792e27aac9ed00735d10165c966b9
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-06-14 23:41:50 +0500

    intersection

--- LinkedList/intersection.cpp
+++ LinkedList/intersection.cpp
@@ -1,66 +1,88 @@
 #include <iostream>
-#include <unordered_set>
 using namespace std;
 
-struct Node {
+class Node{
+public:
     int data;
     Node* next;
-    Node(int val) : data(val), next(nullptr) {}
+
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
 };
 
-Node* getIntersectionNode(Node* head1, Node* head2) {
-    unordered_set<Node*> visited;
+class LinkedList{
+private:
+    Node* head;
 
-    while (head1) {
-        visited.insert(head1);
-        head1 = head1->next;
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insert(int value){
+        Node* newNode = new Node(value);
+        if(!head){
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while(temp->next)
+            temp = temp->next;
+        temp->next = newNode;
     }
 
-    while (head2) {
-        if (visited.find(head2) != visited.end())
-            return head2;
-        head2 = head2->next;
+    void display(){
+        Node* temp = head;
+        while(temp){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
     }
 
-    return nullptr;
-}
-
-// Helper function to print list
-void printList(Node* head) {
-    while (head) {
-        cout << head->data << " -> ";
-        head = head->next;
+    Node* getHead(){
+        return head;
     }
-    cout << "NULL\n";
-}
 
-int main() {
-    // First linked list: 1 -> 2 -> 3 \
-    //                                -> 6 -> 7
-    // Second linked list:     4 -> 5 /
+    void printIntersection(LinkedList& other){
+        Node* p1 = head;
+        Node* p2 = other.getHead();
 
-    Node* common = new Node(6);
-    common->next = new Node(7);
+        cout<<"Intersection: ";
+        while(p1 && p2){
+            if (p1->data == p2->data){
+                cout<<p1->data<<" ";
+                p1 = p1->next;
+                p2 = p2->next;
+            } else if(p1->data < p2->data){
+                p1 = p1->next;
+            } else{
+                p2 = p2->next;
+            }
+        }
+        cout<<endl;
+    }
+};
 
-    Node* head1 = new Node(1);
-    head1->next = new Node(2);
-    head1->next->next = new Node(3);
-    head1->next->next->next = common;
+int main(){
+    LinkedList list1, list2;
 
-    Node* head2 = new Node(4);
-    head2->next = new Node(5);
-    head2->next->next = common;
+    list1.insert(1);
+    list1.insert(2);
+    list1.insert(3);
+    list1.insert(4);
+    list1.insert(6);
 
-    cout << "List 1: ";
-    printList(head1);
-    cout << "List 2: ";
-    printList(head2);
+    list2.insert(2);
+    list2.insert(4);
+    list2.insert(6);
+    list2.insert(8);
 
-    Node* intersection = getIntersectionNode(head1, head2);
-    if (intersection)
-        cout << "Intersection at node with value: " << intersection->data << endl;
-    else
-        cout << "No intersection found.\n";
+    cout<<"List 1: ";
+    list1.display();
 
-    return 0;
+    cout<<"List 2: ";
+    list2.display();
+
+    list1.printIntersection(list2);
 }
